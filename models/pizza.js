d3a473f1282191d8a26b4484bc1a85a0754e3c7b
Jeffrey Johnson <frankp27@me.com>
2022-02-15 21:46:30 -0500

    fully functional

---
+++ models/pizza.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  toppings: []
+});
+
+const pizza = model('Pizza', PizzaSchema);
+
+module.exports =  pizza;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the Pizza model
+module.exports = Pizza;
